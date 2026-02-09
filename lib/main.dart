bc4b3ae962d435cd19bcd594247b0d9d18ae6b15
KevinKuslin <tyrantnitrox@gmail.com>
2026-02-09 13:13:04 +0700

    Rapi-rapiin quiz

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       debugShowCheckedModeBanner: false, 
-      home: const QuizFill()
+      home: const LoginPage()
     ); 
   }
 }
